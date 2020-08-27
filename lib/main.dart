85921d232348003ec41ca4945b1c19b19626aabb
Shuvo <shuvo@Shuvos-MacBook-Air.local>
2020-08-27 22:44:36 +0600

    Added list view with a image & text Added padding to text & image Added divider for list view rows

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,30 @@ class GettingsStartedFlutterApp extends StatelessWidget {
 class ContentView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text("Hello Flutter", style: new TextStyle(fontSize: 22.0),)
+
+    return new ListView.builder(
+      itemCount: 20,
+      itemBuilder: (context, rowNumber) {
+        return new Container(
+          padding: new EdgeInsets.all(16.0),
+          child: new Column(
+            children: <Widget>[
+              new Image.network("https://goo.gl/vFdXGc"),
+              new Container(height: 8.0),
+              new Text("Image Title", style: new TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 18.0),
+                ),
+              new Divider(
+                color: Colors.green,
+              )
+            ],
+          ),
+        );
+      },
     );
+
+    // return new Center(
+    //   child: new Text("Hello Flutter", style: new TextStyle(fontSize: 22.0),)
+    // );
   }
 }
